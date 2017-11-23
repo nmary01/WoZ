d12e3eb9139c279e4b0e96b3c7e7d230f1ee5257
claire <gourdon.claire07@gmail.com>
2017-11-23 09:51:57 +0100

    ajout classe game

--- src/functional core/Game.java
+++ src/functional core/Game.java
@@ -20,30 +20,26 @@ public class Game
 {
     private Parser parser;
     private Room currentRoom;
-        
+    private Room anteroom,ritualroom,cellar;
+    private Room hall,banquetinghall,dancingroom,poolroom,kitchen,garden,well,gardenerhut; 
+    private Room livingroom,library,laboratory,corridor,bedroom,guestbedroom,bathroom;
+    private Room attic;
+    private Neutral mr_Taylor, ms_Taylor, ms_Cunningham;
+    private Ally chambermaid, bob_Taylor, mr_Cunningham, ms_Wellington;
+    private Enemy valet, caretaker, mr_Wellington, nina_Taylor;    
     /**
      * Create the game and initialise its internal map.
      */
     public Game() 
     {
-        createRooms();
         parser = new Parser();
     }
 
     /**
      * Create all the rooms and link their exits together.
      */
-    private void createRooms()
-    {
-        // -1
-        Room anteroom,ritualroom,cellar;
-        // RDC
-        Room hall,banquetinghall,dancingroom,poolroom,kitchen,garden,well,gardenerhut; 
-        // +1 
-        Room livingroom,library,laboratory,corridor,bedroom,guestbedroom,bathroom;
-        // +2
-        Room attic;
-        
+    public void createRooms()
+    {  
         // create the rooms
             //RDC//
         hall = new Room("Hall");
@@ -121,13 +117,224 @@ public class Game
         currentRoom = poolroom;  // start game outside
     }
 
+    public void createCharacters()
+    {
+        
+        
+        Room poolroom = new Room ("PoolRoom");
+        Item item_chambermaid = new Item("Broom","The brush that she uses");
+        Item item_bob_taylor = new Item("Toys","Bob Taylor toys");
+        Item item_mr_Cunningham = new Item("Black hat","The nice hat of Mr Cunningham");
+        Item item_ms_Wellington = new Item("Ring", "This is the wedding ring");
+        
+        // Neutral characters creation //
+        mr_Taylor = new Neutral("Mr Taylor"," ", poolroom);
+        ms_Taylor = new Neutral("Ms Taylor","Oh no ! My poor husband is dead ! What am I going to do now ?!", poolroom);
+        ms_Cunningham = new Neutral("Ms Cunningham","My husband was the best old friend of Mr Taylor", poolroom);
+        
+        // Ally characters creation //
+        chambermaid = new Ally("Chambermaid","I was in the dancing room to serve the guests.I was not with the master", poolroom, item_chambermaid);
+        bob_Taylor = new Ally("Bob Taylor","What happended to my father ? ", poolroom, item_bob_taylor);
+        mr_Cunningham = new Ally("Mr Cunningham", "I was in my bedroom when I heard someone screaming. So I went down stairs quickly", poolroom, item_mr_Cunningham);
+        ms_Wellington = new Ally("Ms Wellington", "I was in the dancing room with my husband. The chambermaid served us.", poolroom, item_ms_Wellington);
+        
+        // Enemy characters ceration //
+        valet = new Enemy("Valet","I was in the kitchen. I checked the wine delivery", poolroom,4,80,12); // damage =4 accuraccy = 80, HP=12
+        caretaker = new Enemy("Caretaker","I was in my hut to repair the fork",poolroom, 2,20,5); // damage =2 accuraccy = 20, HP=5
+        mr_Wellington = new Enemy("Mr Wellington","I was in the dancing room with my wife", poolroom, 1,10,7); // damage =1 accuraccy = 10, HP=7
+        nina_Taylor = new Enemy("Nina Taylor", "I was in the library and I read a good book", poolroom, 2, 30, 8); // damage =2 accuraccy = 30, HP=8
+        
+
+        
+        
+    }
+     public void createItem()
+    {
+        // items in poolroom //
+        
+       Item necklace_red = new Item ("Red Necklace","This is a strange red necklace");
+       poolroom.addItem(necklace_red);
+        
+        // items in dancing room // 
+        Item gramophone = new Item ("Gramophone","This is a nice gramophone but without disk");
+        Item candelar = new Item("Candelar","This candelar gives light and is really beautiful");
+        dancingroom.addItem(gramophone);
+        dancingroom.addItem(candelar);
+         
+        //items in hall //
+        Potion potion2HP = new Potion("Potion 2 HP","This potion gives 2 HP to the player",2);
+        hall.addItem(potion2HP);
+        
+        //items in corridor //
+        Weapon halberd = new Weapon("Halberd","This the statut halberd. It was not a really good idea to take it",4,60);
+        corridor.addItem(halberd);
+        
+        // items in bathroom //
+        Potion potion6HP = new Potion("Potion6HP","This potions gives 6 HP to the player",6);
+        bathroom.addItem(potion6HP);
+        
+        // items in guestbedroom //
+        Exit exit_from_corridor_to_bedroom = new Exit("south",corridor,false);
+        Keys bedroomkey = new Keys("Bedroom key","This key opens the master bedroom door",exit_from_corridor_to_bedroom);
+        guestbedroom.addItem(bedroomkey);
+        
+        // items in kitchen // 
+        Weapon set_of_forks_and_knives = new Weapon("Set of forks and knives","This weapon is a set of silver forks and silver knives",2,40);
+        kitchen.addItem(set_of_forks_and_knives);
+        
+        // items in bedroom //
+        Item disk = new Item("Disk","This disk can be used with the gramophone");
+        bedroom.addItem(disk);
+        
+        Potion potion3HP = new Potion ("Potion3HP","This potions gives 3 HP to the player",3);
+        bedroom.addItem(potion3HP);
+        
+        Item money = new Item("Money","You find 60 pounds");
+        bedroom.addItem(money);
+        
+        // items in the library //
+        Item book = new Item("Book","The title book is 'The best human friend : the cat'");
+        library.addItem(book);
+        
+        // items in laboratory //
+        Exit exit_from_corridor_to_attic = new Exit("up",corridor,false);
+        Keys attickey = new Keys("Attic key","This key is a shaped bone from a squeletor", exit_from_corridor_to_attic); 
+        laboratory.addItem(attickey);
+        
+        Item construction_drawing = new Item("Construction drawing","You find construction drawings. Mr Taylor planed to dig really deeply to the east of the gardener hut");
+        laboratory.addItem(construction_drawing);
+        
+        //items in garden //
+        Weapon fork = new Weapon("Fork","This fork is green and brown",3,50);
+        garden.addItem(fork);
+        
+        Potion apple = new Potion("Apple","This apples gives you 2 HP",2);
+        garden.addItem(apple);
+        
+        // items in gardenerhut //
+        
+        Item pliers = new Item("Pliers","You can cut a chain with this object");
+        gardenerhut.addItem(pliers);
+        
+        Item ritual_cape = new Item("Ritual cape","You find a black ritual cape. It is very strange !");
+        gardenerhut.addItem(ritual_cape);
+        
+        Item necklace_black_1 = new Item("Necklace","You find a very strange necklace ! It is a black rond necklace with the same symbol than in the ritual cape");
+        gardenerhut.addItem(necklace_black_1);
+        
+       //items in anteroom //
+       
+       Potion potion4HP = new Potion("Potion4HP","This potion gives 4 HP to the player",4);
+       anteroom.addItem(potion4HP);
+       
+       Weapon sword = new Weapon("Sword","A really sharp sword",6,70);
+       anteroom.addItem(sword);
+       // items in ritual room //
+       
+       Item red_ritual_cape = new Item("Red ritual cape", "This is a ritual cape such as in the gardener hut but this one is red. There are some blond curly hairs");
+       ritualroom.addItem(red_ritual_cape);
+       
+       Item black_ritual_cape_1 = new Item("Black ritual cape","This is a black ritual cape");
+       ritualroom.addItem(black_ritual_cape_1);
+       
+       Item black_ritual_cape_2 = new Item ("Black ritual cape","Another black ritual cape");
+       ritualroom.addItem(black_ritual_cape_2);
+       
+    }
+    
+        
     /**
      *  Main play routine.  Loops until end of play.
      */
     public void play() 
     {            
         printWelcome();
-
+        createRooms();
+        createCharacters();
+        createItem();
+        Player player = new Player("player");
+        
+        // Step 1 : start in the poolroom //
+            // All the characters are in the poolroom and the exit opens only after speak with all the characters //
+        Boolean speak_with_mr_Taylor, speak_with_ms_Taylor, speak_with_ms_Cunningham, speak_with_chambermaid, speak_with_bob_Taylor, speak_with_mr_Cunningham = false;
+        Boolean speak_with_ms_Wellington, speak_with_valet, speak_with_caretaker, speak_with_mr_Wellington, speak_with_nina_Taylor;
+        Boolean exam_room, exam_dead;
+        
+        speak_with_mr_Taylor =false;
+        speak_with_ms_Taylor = false;
+        speak_with_ms_Cunningham = false;
+        speak_with_chambermaid = false;
+        speak_with_bob_Taylor = false;
+        speak_with_mr_Cunningham = false;
+        speak_with_ms_Wellington = false;
+        speak_with_valet = false;
+        speak_with_caretaker = false;
+        speak_with_mr_Wellington = false;
+        speak_with_nina_Taylor = false;
+        exam_room = false;
+        exam_dead = false;
+        
+        if (speak_with_mr_Taylor && speak_with_ms_Taylor && speak_with_ms_Cunningham && speak_with_chambermaid && speak_with_bob_Taylor && speak_with_mr_Cunningham &&
+            speak_with_ms_Wellington && speak_with_valet && speak_with_caretaker && speak_with_mr_Wellington && speak_with_nina_Taylor && exam_room && exam_dead)
+            // Doit ajouter en plus que le joueur doit examiner la pièce et le corps
+            {
+                poolroom.modifyExit("east");
+            }
+        
+        
+         // Step 2 : banqueting hall //
+         Boolean toys_not_front_of_door = false; //false = toys front of the door // true = toys not front of the door
+         Boolean find_bob_Taylor = false;
+         System.out.println(" When you opened the poolroom door, you shut on a toy so you lose one HealthPoint");
+         player.setHP(player.getHP()-1);
+         System.out.println("You cannot open the dancing room door now because there are a lot of toys");
+         nina_Taylor.setRoom(banquetinghall);                 
+         nina_Taylor.setText("These are my brother toys. Find him to tidy his toys");
+         
+         // Step 2 bis : Hall //
+         //if the player examine the place
+         System.out.println(" You are in the hall but there is nobody here. ");
+         
+         // Step 3 : kitchen //
+         //if the player examine the place
+         System.out.println(" This is the kitchen, Bob Taylor is not here. ");
+         System.out.println(" A set of forks and knives is in one of the cupboards of the kitchen. ");
+         
+         // Step 4 : cellar //
+         //if the player examine the place
+         System.out.println("You found Bob Taylor in the back of the cellar");
+         find_bob_Taylor = true;
+         toys_not_front_of_door = true;
+         
+         // Step 5 :  banqueting hall //
+         if(toys_not_front_of_door)
+         {
+            banquetinghall.modifyExit("south");
+            
+         }
+         // the player can now open the dancing room door
+                          
+         // Step 6 : dancing room //
+         speak_with_ms_Cunningham= false;
+         
+         //if the player examine the place
+         System.out.println("");
+        
+         ms_Cunningham.setText("Oh, good morning ! I am happy to see you. Unlike the dark atmosphere of the mansion it is a beautiful weather outside. \n" + 
+         		"I hope your investigation is progressing well. I am scared that the murderer is still at liberty. I cannot sleep properly since the night of the murder. Ah ! I know, you are going to dance with me. It will make me think of something other than this dark situation. \n" + 
+         		"Sorry I will walk on your feet. Do I hurt you not too much ? I can be very clumsy.Oh, good morning ! I am happy to see you. Unlike the dark atmosphere of the mansion it is a beautiful weather outside. \n" + 
+         		"I hope your investigation is progressing well. I am scared that the murderer is still at liberty. I cannot sleep properly since the night of the murder. Ah ! I know, you are going to dance with me. It will make me think of something other than this dark situation. \n" + 
+         		"Sorry I will walk on your feet. Do I hurt you not too much ? I can be very clumsy.");
+         
+            if (speak_with_ms_Cunningham)
+         {
+            player.setHP(player.getHP()-1);
+            System.out.println("You spoke and danced a lot of time with Ms Cunningham. Then you are very tired so you lose one healthpoint");
+         
+        }
+         
+         
+         
         // Enter the main command loop.  Here we repeatedly read commands and
         // execute them until the game is over.
                 
