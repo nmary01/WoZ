318d390f73f75851544ee994aa681165e77e5a26
claire <gourdon.claire07@gmail.com>
2017-12-15 07:27:41 +0100

    modif com PNG, ally, enemy, neutral, exit

--- src/core/PNG.java
+++ src/core/PNG.java
@@ -6,6 +6,7 @@ package core;
  * - a name
  * - a text
  * - a room
+ * - a picture
  * @author (Grp4WoZ)
  * @version (20/11/2017)
  */
@@ -14,11 +15,15 @@ package core;
     private String name; //The name of the PNG
     private String text; //The text said by the PNG
     protected Room room; //The room where is the PNG
-    private String picture;
+    private String picture; //The picture of the PNG
 
 
     /**
-     * Constructeur d'objets de classe PNG
+     * Objects constructor of the class PNG
+     * @param String name    name of the PNG
+     * @param String text     words he can express
+     * @param Room room     place where he is
+     * @param String picture     PNG's picture
      */
     public PNG(String name, String text, Room room, String picture)
     {     
@@ -66,13 +71,16 @@ package core;
     /**
      * Method setRoom
      *
-     * @param text the new text of the PNG
+     * @param room the new room where the PNG is located
      */
     public void setRoom(Room room)
     {
         this.room = room;
     }
 
+    /**
+     * Return the picture of the PNG 
+     */
     public String getPicture() {
         return picture;
     }
