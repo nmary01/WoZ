1c8ab13978de5b536ad8a12439ff2f67a40d7ea7
Cecile <cecile.iaegi@gmail.com>
2017-12-15 09:15:47 +0100

    Potion comments

--- src/core/Potion.java
+++ src/core/Potion.java
@@ -7,8 +7,10 @@ import java.util.*;
  * - a description (5+ characters)
  * - heal (the number of HP it gives back)
  * - used (if the potion is empty or not)
+ * 
+ * This class is a subclass of Item class
  * @author (WoZGrp4)
- * @version (20/11/2017)
+ * @version (14/12/2017)
  */
 public class Potion extends Item
 {
@@ -22,7 +24,7 @@ public class Potion extends Item
      * Constructor for objects of class Potion
      * @param potionName. The name of the potion
      * @param potionDescription. The description of the potion
-     * @param s. The number of HP that the potion gives
+     * @param heal. The number of HP that the potion gives
      */
     public Potion(String potionName,String potionDescription, int heal)
     {
